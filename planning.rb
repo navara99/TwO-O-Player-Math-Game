44c68d311aed1306b56a7d0818b859f7424747ab
Tharsikan <thar.navaratnam@gmail.com>
2022-01-03 00:43:10 +0000

    fix planning doc

--- planning.rb
+++ planning.rb
@@ -12,7 +12,7 @@
   
   The game class will be an instance of a game. This class will contain the game loop. 
   Game will have a current_player instance variable. The game class will have a play method that starts
-  the game loop. It will also have a private ask method which generates the question. It will also have a new method that 
+  the game loop. It will also have a private ask method which generates the question. The initialize method in the Game class
   creates 2 instances of the Player class (player1, player2) and resets the score.
   
 =end
