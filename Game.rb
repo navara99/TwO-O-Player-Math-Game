74f82e4b4b4260f7cea6937d568bd8543fa9624a
Tharsikan <thar.navaratnam@gmail.com>
2022-01-03 01:24:29 +0000

    implement ask function

--- Game.rb
+++ Game.rb
@@ -6,22 +6,26 @@ class Game
     @player_1 = Player.new
     @player_2 = Player.new
     @current_player = @player_1
-    puts "New game intialized. Call the play method to start playing."
+    puts "New game intialized."
   end
 
   def play
 
-    ask()
-    # while @player_1.lives && @player_2.lives
-    #   answer = gets.chomp
-    # end
+    while (@player_1.lives != 0) && (@player_2.lives != 0)
+      correct = ask()
+      @current_player.lives -= 1 unless correct
+    end
 
   end
 
   private
 
   def ask
-    
+    num_1 = rand(1...20)
+    num_2 = rand(1...20)
+    puts "What does #{num_1} plus #{num_2} equal?"
+    answer = gets.chomp
+    answer == num_1 + num_2
   end
 
 end
