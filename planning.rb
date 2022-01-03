d9226d4807277134dd9d52cd5520f0de01a2f067
Tharsikan <thar.navaratnam@gmail.com>
2022-01-03 00:35:29 +0000

    initial commit

---
+++ planning.rb
@@ -0,0 +1,19 @@
+=begin
+
+  Possible Classes
+  ******************************
+  Player
+  Game
+
+  Roles
+  ******************************
+  The player class will create an instance of each player. It will have an initialize method and have instance
+  variable to represent the number of lives. There will be no user inputs while creating an instance of this class
+  
+  The game class will be an instance of a game. This class will contain the game loop. 
+  Game will have a current_player instance variable. The game class will have a play method that starts
+  the game loop. It will also have an ask method which generates the question. It will also have a new method that 
+  creates 2 instances of the Player class (player1, player2).
+  
+
+=end
