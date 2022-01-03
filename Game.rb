1422aa83a5326faad8c3b8d6559fd056b0e80da2
Tharsikan <thar.navaratnam@gmail.com>
2022-01-03 01:05:36 +0000

    setup play and ask method

--- Game.rb
+++ Game.rb
@@ -2,8 +2,26 @@ require_relative "Player"
 
 class Game
 
-def initialize
-  puts "Game intialized"
-end
+  def initialize
+    @player_1 = Player.new
+    @player_2 = Player.new
+    @current_player = @player_1
+    puts "New game intialized. Call the play method to start playing."
+  end
+
+  def play
+
+    ask()
+    # while @player_1.lives && @player_2.lives
+    #   answer = gets.chomp
+    # end
+
+  end
+
+  private
+
+  def ask
+    
+  end
 
 end
